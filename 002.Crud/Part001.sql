71fe98d221c10a8c2d52e251741e9f02cb804ba9
NeonCardinal <nvnnaidenov@gmail.com>
2025-09-17 23:23:01 +0300

    add

---
+++ 002.Crud/Part001.sql
@@ -0,0 +1,20 @@
+USE [SoftUni]
+GO
+
+--Task 002
+SELECT * FROM [Departments]
+
+--Task 003
+SELECT [Name] FROM [Departments]
+
+--Task 004
+SELECT [FirstName],
+	   [LastName],
+	   [Salary]
+  FROM [Employees]
+
+--Task 005
+SELECT [FirstName],
+	   [MiddleName],
+	   [LastName]
+  From [Employees]
